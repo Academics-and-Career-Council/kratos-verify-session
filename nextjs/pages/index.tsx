ff2fb5a5b84b05bafcdf328781ed2a31933ca40e
Mshivam2409 <shivammalhotra08@gmail.com>
2021-09-06 14:22:44 +0530

    Initial Commit

---
+++ nextjs/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+import { ExampleComponent } from 'kratos-verify-session'
+const Home: NextPage = () => {
+  return <ExampleComponent text={'NextJS'} />
+}
+
+export default Home
