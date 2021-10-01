ac5c04c21add21e5eccbb6bcf449410e8f53eb03
Faheem <85359396+Faheem-Nizar@users.noreply.github.com>
2021-10-01 22:39:15 +0530

    Added and edited Loader and CSS

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,11 +1,11 @@
-import styles from '../styles/loader.module.css'
-import Image from 'next/image'
+import styles from './styles.module.css'
+import React from 'react';
 
 export default function loader(){
     return(
         <div className={styles.container}>
             <div className={styles.logo}>
-            <Image src="/anc-logo.png" width=
+            <img src="/anc-logo.png" width=
         {150} height={150}/>
             </div>
 
